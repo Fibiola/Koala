f8975f1cb19b1a342925ca6df3c2908d6802285b
Fibiola <natasha.pristovsek@gmail.com>
2013-10-31 21:31:54 +0100

    database users added

--- tweety/models.py
+++ tweety/models.py
@@ -1,17 +1,28 @@
 from django.db import models
-#we imported the models from DB library 
+import datetime
+from django.utils import timezone
+
+# We imported the models from DB library 
 # Create your models here.
 
-#It tells us that new class Post  inharits some of the properties from models.Model.
+# It tells us that new class Post  inharits some of the properties from models.Model.
 #model has title, body, publish date
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     pub_date = models.DateTimeField('date published')
     likes = models.IntegerField(default=0)
     
+    def __unicode__(self): 
+        return self.title
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
 class User(models.Model):
     post = models.ForeignKey(Post)
-    post_text = models.CharField(max_length=200)
+    user_description = models.CharField(max_length=200)
     
-    
+    def __unicode__(self):
+        return self.user_description
