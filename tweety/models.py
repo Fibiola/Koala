60d1ee1a963d6de15c7292201d2361413f1aec30
Fibiola <natasha.pristovsek@gmail.com>
2013-10-31 04:55:40 +0100

    database and classes added

--- tweety/models.py
+++ tweety/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+#we imported the models from DB library 
 # Create your models here.
+
+#It tells us that new class Post  inharits some of the properties from models.Model.
+#model has title, body, publish date
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    likes = models.IntegerField(default=0)
+    
+class User(models.Model):
+    post = models.ForeignKey(Post)
+    post_text = models.CharField(max_length=200)
+    
+    
