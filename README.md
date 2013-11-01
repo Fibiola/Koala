2fc305969643160f39b34053e31ec1ec932c0234
Fibiola <natasha.pristovsek@gmail.com>
2013-11-01 18:16:35 +0100

    changed readme, added admin

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## Koala app
 using Django framework 
+
+![alt tag](http://i.imgur.com/mlCFOqB.gif)
---
+++ tweety/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from tweety.models import Post
+
+admin.site.register(Post)
