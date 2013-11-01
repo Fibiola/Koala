4e054b6d047dbbf6191498a1fa76c79af63f8d27
Fibiola <natasha.pristovsek@gmail.com>
2013-11-01 19:29:47 +0100

    changed Admin form for users

--- tweety/admin.py
+++ tweety/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from tweety.models import Post
+from tweety.models import User
+
+admin.site.register(User)
+
+class UserInline(admin.TabularInline):
+    model = User
+    extra = 1
+    
 
 class PostAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -8,4 +16,6 @@ class PostAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date']}),
         ('Popularity',      {'fields': ['likes'], 'classes': ['collapse']}),
     ]
+    inlines = [UserInline]
+    list_display = ('title', 'body', 'pub_date', 'likes', 'was_published_recently')
 admin.site.register(Post, PostAdmin)
